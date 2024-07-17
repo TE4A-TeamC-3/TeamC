623993a7d3ef39629e9b71d6ab907f3c94a6049f
hinata Takeda <210011@jc-21.jp>
2024-07-17 12:54:13 +0900

    更新

--- src/test/java/jp/te4a/mybootapp6/Mybootapp6ApplicationTests.java
+++ src/test/java/jp/te4a/mybootapp6/Mybootapp6ApplicationTests.java
@@ -3,10 +3,10 @@ package jp.te4a.mybootapp6;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = Mybootapp6Application.class)
+//@SpringBootTest(classes = Mybootapp6Application.class)
 class Mybootapp6ApplicationTests {
 
-    @Test
+    //@Test
     void contextLoads() {
     }
 
