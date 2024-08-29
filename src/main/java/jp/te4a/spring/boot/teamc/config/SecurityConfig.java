4d64889ea92e32ca402068b1e9e9b82d08f15382
te210008 <te210008@gmail.com>
2024-08-29 10:51:09 +0900

    備品管理システム

--- src/main/java/jp/te4a/spring/boot/teamc/config/SecurityConfig.java
+++ src/main/java/jp/te4a/spring/boot/teamc/config/SecurityConfig.java
@@ -41,6 +41,8 @@ public class SecurityConfig {
                 .requestMatchers("/users").permitAll()
                 .requestMatchers("/admin/**").hasRole("ADMIN") // ADMINロールのみアクセス可
                 .requestMatchers("/user/**").hasRole("USER") // USERロールのみアクセス可
+                //tools/listの権限を付与して
+                .requestMatchers("/tools/list").permitAll()
                 .requestMatchers(HttpMethod.POST, "/tools/create").hasRole("ADMIN") // POSTメソッドの/tools/createに対してADMINロールを要求
                 .requestMatchers(HttpMethod.POST, "/tools/delete").hasRole("ADMIN") // POSTメソッドの/tools/deleteに対してADMINロールを要求
                 .requestMatchers(HttpMethod.POST, "/tools/edit").hasRole("ADMIN") // POSTメソッドの/tools/editに対してADMINロールを要求
