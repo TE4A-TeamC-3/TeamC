df863abc79c545074d85b2841ee5f66ae586a572
hinata Takeda <210011@jc-21.jp>
2024-09-20 11:26:57 +0900

    db接続不能のためコピー

--- src/main/java/jp/te4a/spring/boot/teamc/ToolApplication.java
+++ src/main/java/jp/te4a/spring/boot/teamc/ToolApplication.java
@@ -1,15 +1,14 @@
-package jp.te4a.spring.boot.myapp10;
+package jp.te4a.spring.boot.teamc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //SpringBootのメインクラス
 @SpringBootApplication
-public class BookApplication {
+public class ToolApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookApplication.class, args);
+		SpringApplication.run(ToolApplication.class, args);
 	}
 
 }
-
