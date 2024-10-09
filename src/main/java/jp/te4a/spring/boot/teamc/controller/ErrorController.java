e5397044afda0991b0948121503afedd78e5bbc6
hinata Takeda <210011@jc-21.jp>
2024-10-09 11:30:08 +0900

    備品管理121

--- src/main/java/jp/te4a/spring/boot/teamc/controller/ErrorController.java
+++ src/main/java/jp/te4a/spring/boot/teamc/controller/ErrorController.java
@@ -1,18 +1,24 @@
+import org.springframework.boot.web.servlet.error.ErrorController; // ErrorControllerをインポート
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ErrorController {
+public class CustomErrorController implements ErrorController { // ErrorControllerを実装
 
     @GetMapping("/error")
     public String accessDenied() {
         System.out.println("ErrorController");
-        return "access-denied";
+        return "access-denied";  // access-denied.htmlを返す
     }
 
     @ExceptionHandler(Exception.class)
     public String handleGlobalException() {
-        return "error";
+        return "error";  // error.htmlを返す
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error"; // これをオーバーライドしてエラーハンドリングのパスを指定
     }
 }
