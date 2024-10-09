6e97267ca6d611ce8cdc0912dff7e185c42ef846
hinata Takeda <210011@jc-21.jp>
2024-10-09 11:26:08 +0900

    備品管理120

--- src/main/java/jp/te4a/spring/boot/teamc/controller/ErrorController.java
+++ src/main/java/jp/te4a/spring/boot/teamc/controller/ErrorController.java
@@ -1,4 +1,5 @@
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
