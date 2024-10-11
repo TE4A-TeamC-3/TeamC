c8d30caea5fa1ff6286743cdd5efa3755bca1add
hinata Takeda <210011@jc-21.jp>
2024-10-11 11:36:08 +0900

    備品管理144

--- src/main/java/jp/te4a/spring/boot/teamc/service/ToolService.java
+++ src/main/java/jp/te4a/spring/boot/teamc/service/ToolService.java
@@ -12,8 +12,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Sort;
 
 import jp.te4a.spring.boot.teamc.bean.ToolBean;
-import jp.te4a.spring.boot.teamc.bean.ToolForm;
 import jp.te4a.spring.boot.teamc.bean.DeleteListBean;
+import jp.te4a.spring.boot.teamc.form.ToolForm;
 import jp.te4a.spring.boot.teamc.repository.ToolRepository;
 import jp.te4a.spring.boot.teamc.repository.DeleteListRepository;
 
